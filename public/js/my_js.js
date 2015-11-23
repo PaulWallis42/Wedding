febe22b4576696b36062d308954b1bb2dd3e494a
paulwallis42 <paulwallis42@gmail.com>
2015-11-23 17:32:48 +0000

    menu bar

--- public/js/my_js.js
+++ public/js/my_js.js
@@ -3,8 +3,28 @@ function div_show() {
 document.getElementById('abc').style.display = "block";
 }
 
-
 //Function to Hide Popup
 function div_hide(){
 document.getElementById('abc').style.display = "none";
 }
+
+
+//Drop down Menu
+
+( function( $ ) {
+$( document ).ready(function() {
+// Cache the elements we'll need
+var menu = $('#cssmenu');
+var menuList = menu.find('ul:first');
+var listItems = menu.find('li').not('#responsive-tab');
+
+// Create responsive trigger
+menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+
+// Toggle menu visibility
+menu.on('click', '#responsive-tab', function(){
+	listItems.slideToggle('fast');
+	listItems.addClass('collapsed');
+});
+});
+} )( jQuery );
