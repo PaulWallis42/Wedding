c0d6ddd8c8be2c3295487fc818644a9097df30f0
paulwallis42 <paulwallis42@gmail.com>
2015-11-23 17:04:27 +0000

    pop up form clean up

--- public/js/my_js.js
+++ public/js/my_js.js
@@ -1,12 +1,3 @@
-// Validating Empty Field
-function check_empty() {
-if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-alert("Fill All Fields !");
-} else {
-document.getElementById('form').submit();
-alert("Form Submitted Successfully...");
-}
-}
 //Function To Display Popup
 function div_show() {
 document.getElementById('abc').style.display = "block";
