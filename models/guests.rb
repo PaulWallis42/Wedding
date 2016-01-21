81bd63cba3661a1a991463a569bff9086de38536
paulwallis42 <paulwallis42@gmail.com>
2016-01-21 09:14:09 +0000

    potentially db fix

--- models/guests.rb
+++ models/guests.rb
@@ -11,7 +11,6 @@ class Guest
   # these property declarations set the column headers in the Link table
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :name,  String
-  property :attending, String
   property :others, String
   property :guest1_menu, String
   property :partner_name, String
@@ -22,6 +21,7 @@ class Guest
   property :child_2_menu, String
   property :bbq, String
   property :additional, String, :length => 1000
+  property :attending, String
 end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
